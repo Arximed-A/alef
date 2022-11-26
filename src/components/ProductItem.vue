5534ecc929f47d5251f125c4171068e234c31fb9
AndrewArkhipov <AndrewArlhipow@yandex.ru>
2022-11-26 17:29:17 +0300

    init+add img pijamas

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="product">
+    <div class="product__images"></div>
+    <div class="product__description"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ProductItem",
+};
+</script>
+
+<style></style>
